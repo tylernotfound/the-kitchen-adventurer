011b20e35cdd65564ca683ff762722bc34f6dc1f
Tyler Smith <hello@tylerbuilds.co>
2015-10-08 19:50:39 -0700

    Adding pinterest verification

--- wp-content/themes/tka/header.php
+++ wp-content/themes/tka/header.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="p:domain_verify" content="108c868ff12f2d92c2764a393fb81701"/>
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico?v=12">
